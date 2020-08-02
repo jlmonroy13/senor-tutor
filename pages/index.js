1efd80f1668ddd95b4697cdbcd10e168c79a4075
Andres Chavez <Andres.F.Chavez.-ND@abc.com>
2020-08-02 17:53:09 -0500

    Required changes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
 
-import Header from 'components/Header';
-
 export default function Home() {
   return (
     <div className="container">
@@ -10,8 +8,22 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <h1>Home Page</h1>
+
+      <div className="home">
+        <div className="home__header">
+          <span className="home__header-logo">Señor Tutor</span>
+          <div className="home__header-sections">
+            <span className="home__header-tag">Cursos</span>
+            <span className="home__header-tag">Youtube</span>
+          </div>
+          <div className="home__header-login">
+            <span className="home__header-tag">Ingresar</span>
+            <span className="home__header-tag  home__header-tag--register">
+              Registrarse
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
