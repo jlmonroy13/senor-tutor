42a8c47c24c2e09dd17974fdef618c291e2d82d8
Andres Chavez <Andres.F.Chavez.-ND@abc.com>
2020-08-02 22:40:07 -0500

    Main view of Home Page

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,32 @@ export default function Home() {
             </span>
           </div>
         </div>
+
+        <div className="home__main">
+          <div className="home__main-content">
+            <h1 className="home__main-content-title">Atrévete a ser mejor</h1>
+            <span className="home__main-content-description">
+              Hay muchos caminos para el éxito estudiantil y nosotros somos uno
+              de ellos.
+            </span>
+            <span className="home__main-content-subdescription">
+              - Omar Fajardo
+            </span>
+            <span className="home__main-content-link">ver cursos</span>
+          </div>
+
+          <div className="home__main-images">
+            <div className="home__main-images-frame" />
+            <img
+              className="home__main-images-photo"
+              src="/images/tutor.png"
+              alt="tutor"
+            />
+            <div className="home__main-images-tag">
+              <span className="home__main-images-tag-text">Señor Tutor</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
